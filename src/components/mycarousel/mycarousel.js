a10bcd7282f6015adb7fa3fd0fb951cae0dbdd71
Monica <32903674+monicaafilip@users.noreply.github.com>
2021-11-27 18:28:33 +0200

    added action for button

--- src/components/mycarousel/mycarousel.js
+++ src/components/mycarousel/mycarousel.js
@@ -1,4 +1,5 @@
-import React, { Component } from "react";
+import React from "react";
+import { useHistory } from "react-router";
 
 import { Carousel } from "react-bootstrap";
 
@@ -7,30 +8,34 @@ import "./mycarousel.css";
 import firstImg from "../../assets/temporary/products/prod (1).jpeg";
 import secondImg from "../../assets/temporary/products/prod (2).jpeg";
 
-class MyCarousel extends Component {
-  render() {
-    return (
-      <Carousel>
-        <Carousel.Item interval={2000}>
-          <img className="d-block w-100" src={firstImg} alt="First slide" />
-          <Carousel.Caption>
-            <h1 className="display-4 textCarousel h1VWSize">Modern Design</h1>
-            <p className=" normalVWSize textCarousel">
-              Mobilă second-hand import Italia
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item interval={2000}>
-          <img className="d-block w-100" src={secondImg} alt="Second slide" />
-          <Carousel.Caption>
-            <h1 className="display-4 h1VWSize">Modern Design</h1>
-            <button className="btn btn-success my-2 my-sm-0  productsBtn normalVWSize">
-              Produse
-            </button>
-          </Carousel.Caption>
-        </Carousel.Item>
-      </Carousel>
-    );
+function MyCarousel() {
+  const history = useHistory();
+  const routeChange = () => {
+    let path = '/produse';
+    history.push(path);
   }
+
+  return (
+    <Carousel>
+      <Carousel.Item interval={2000}>
+        <img className="d-block w-100" src={firstImg} alt="First slide" />
+        <Carousel.Caption>
+          <h1 className="display-4 textCarousel h1VWSize">Modern Design</h1>
+          <p className=" normalVWSize textCarousel">
+            Mobilă second-hand import Italia
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item interval={2000}>
+        <img className="d-block w-100" src={secondImg} alt="Second slide" />
+        <Carousel.Caption>
+          <h1 className="display-4 h1VWSize">Modern Design</h1>
+          <button className="btn btn-success my-2 my-sm-0  productsBtn normalVWSize" onClick={routeChange}>
+            Produse
+          </button>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
+  );
 }
 export default MyCarousel;
