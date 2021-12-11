43e78e85413754614f16e46c1a2a6bc425b848a6
Monica <32903674+monicaafilip@users.noreply.github.com>
2021-12-11 16:24:11 +0200

    fixed showing location on mobile

--- src/components/media-objects/media-object-left.js
+++ src/components/media-objects/media-object-left.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import './media-object.css';
+import React from "react";
+import "./media-object.css";
 
-const MediaObjectLeft = () => 
-{
-    return (
-        <div className="media-object">
-            <div className="icon-left">
-            <iframe title='Modern design cajvana' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2685.514826891853!2d26.004271950756422!3d47.69384158983909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4734534a7bf7d84b%3A0x8586e04a327e5d00!2sModern%20design!5e0!3m2!1sen!2sro!4v1639141495742!5m2!1sen!2sro" width="600" height="450" allowFullScreen="" loading="lazy"/>
-            </div>
-            <div className="blurb-wrapper-right">
-                <h3 className="blurb-header">Magazin Cajvana
-                </h3>
-                <div className="blurb-info">
-                    <h5>Adresă</h5>
-                    <p>
-                        Cajvana, nr 2058
-                        jud. Suceava, Romania
-                    </p>
-                    <h5>Contact</h5>
-                    <p>
-                        +40746 086 433
-                    </p>
-                </div>
-            </div>
+const MediaObjectLeft = () => {
+  return (
+    <div className="media-object">
+      <div className="icon-left">
+        <iframe
+          title="Modern design cajvana"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2685.514826891853!2d26.004271950756422!3d47.69384158983909!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4734534a7bf7d84b%3A0x8586e04a327e5d00!2sModern%20design!5e0!3m2!1sen!2sro!4v1639141495742!5m2!1sen!2sro"
+          width="100%"
+          height="450"
+          allowFullScreen=""
+          loading="lazy"
+        />
+      </div>
+      <div className="blurb-wrapper-right">
+        <h3 className="blurb-header">Magazin Cajvana</h3>
+        <div className="blurb-info">
+          <h5>Adresă</h5>
+          <p>Cajvana, nr 2058 jud. Suceava, Romania</p>
+          <h5>Contact</h5>
+          <p>+40746 086 433</p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default MediaObjectLeft;
--- src/components/media-objects/media-object-right.js
+++ src/components/media-objects/media-object-right.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import './media-object.css';
+import React from "react";
+import "./media-object.css";
 
-const MediaObjectRight = () => 
-{
-    return (
-        <div className="media-object">
-            <div className="icon-right">
-            <iframe title='Modern Design Danila' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2682.7623600972724!2d26.09203245075841!3d47.74726538614207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473457f90537e11d%3A0xd61a3443dbb6c15a!2sModern%20Design!5e0!3m2!1sen!2sro!4v1639141822473!5m2!1sen!2sro" width="600" height="450" allowFullScreen="" loading="lazy"/>
-            </div>
-            <div className="blurb-wrapper-left">
-                <h3 className="blurb-header">Magazin Dănila</h3>
-                <div className="blurb-info">
-                    <h5>Adresă</h5>
-                    <p>
-                        Dănila, str. Europeana
-                        jud. Suceava, Romania
-                    </p>
-                    <h5>Contact</h5>
-                    <p>
-                        +40746 086 433
-                    </p>
-                </div>
-            </div>
+const MediaObjectRight = () => {
+  return (
+    <div className="media-object">
+      <div className="icon-right">
+        <iframe
+          title="Modern Design Danila"
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2682.7623600972724!2d26.09203245075841!3d47.74726538614207!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x473457f90537e11d%3A0xd61a3443dbb6c15a!2sModern%20Design!5e0!3m2!1sen!2sro!4v1639141822473!5m2!1sen!2sro"
+          width="100%"
+          height="450"
+          allowFullScreen=""
+          loading="lazy"
+        />
+      </div>
+      <div className="blurb-wrapper-left">
+        <h3 className="blurb-header">Magazin Dănila</h3>
+        <div className="blurb-info">
+          <h5>Adresă</h5>
+          <p>Dănila, str. Europeana jud. Suceava, Romania</p>
+          <h5>Contact</h5>
+          <p>+40746 086 433</p>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default MediaObjectRight;
