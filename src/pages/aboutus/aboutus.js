db7049871af9513ec7b590805c6417ed1cdbfb52
Monica <32903674+monicaafilip@users.noreply.github.com>
2021-12-14 11:00:00 +0200

    changed language + text formatting

--- src/pages/aboutus/aboutus.js
+++ src/pages/aboutus/aboutus.js
@@ -1,36 +1,33 @@
-import React from 'react';
+import React from "react";
 import { connect } from "react-redux";
 
-import { createStructuredSelector } from 'reselect';
+import { createStructuredSelector } from "reselect";
 
-import MyNavbar from '../../components/mynavbar/mynavbar';
-import TitleTextContainer from '../../components/title-text-container/title-text-container';
-import Footer from '../../components/footer/footer';
+import MyNavbar from "../../components/mynavbar/mynavbar";
+import TitleTextContainer from "../../components/title-text-container/title-text-container";
+import Footer from "../../components/footer/footer";
 
-import { selectText } from '../../redux/aboutus/aboutus.selectors';
+import { selectText } from "../../redux/aboutus/aboutus.selectors";
 
-import './aboutus.css';
-import '../../App.css';
+import "./aboutus.css";
+import "../../App.css";
 
-const AboutUs = ({text}) => {
-  console.log(text);
+const AboutUs = ({ text }) => {
   return (
     <div className="App">
-        <div className="container-fluid mainHomePage">
-        <MyNavbar/>
-        {
-          text.map(({ id, ...otherProps }) => (
-            <TitleTextContainer key={id}{...otherProps}/>
-          ))
-        }
-        <Footer/>
-        </div>
+      <div className="container-fluid mainHomePage">
+        <MyNavbar />
+        {text.map(({ id, ...otherProps }) => (
+          <TitleTextContainer key={id} {...otherProps} />
+        ))}
+        <Footer />
+      </div>
     </div>
   );
-}
+};
 
 const mapStateToProps = createStructuredSelector({
-  text: selectText
+  text: selectText,
 });
 
 export default connect(mapStateToProps)(AboutUs);
