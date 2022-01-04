7bb8e07c0b4f41a4a8c74be57f1bebb970bc624f
Monica <32903674+monicaafilip@users.noreply.github.com>
2022-01-04 11:45:05 +0200

    moved init for react ga from index.js to app.js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { 
-  HashRouter as Router,
-  Route, 
-  Switch
- } from 'react-router-dom';
- 
-import Home from './pages/home/home';
-import AboutUs from './pages/aboutus/aboutus';
-import Directory from './pages/directory/directory';
+import React from "react";
+import ReactGA from "react-ga";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-import './App.css';
+import Home from "./pages/home/home";
+import AboutUs from "./pages/aboutus/aboutus";
+import Directory from "./pages/directory/directory";
 
-function App() {
-  return (
-    <Router>
-      <Switch>
-        <Route exact path="/acasa" component={Home} />
-        <Route exact path="/" component={Home}/>
-        <Route exact path="/desprenoi" component={AboutUs} />
-        <Route exact path="/produse" component={Directory} />
-		  </Switch>
-    </Router>
-  );
+import "./App.css";
+
+ReactGA.initialize("G-EPCWKYEL85");
+
+class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <Route exact path="/acasa" component={Home} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/desprenoi" component={AboutUs} />
+          <Route exact path="/produse" component={Directory} />
+        </Switch>
+      </Router>
+    );
+  }
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import ReactGA from "react-ga";
 
 import App from "./App";
 import { Provider } from "react-redux";
@@ -9,8 +8,6 @@ import { PersistGate } from "redux-persist/integration/react";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import { store, persistor } from "./redux/store.js";
-
-ReactGA.initialize("G-EPCWKYEL85");
 
 ReactDOM.render(
   <Provider store={store}>
