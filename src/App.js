cce507bd099393c4a2e791c7d6440f9fcd6f2842
Monica <32903674+monicaafilip@users.noreply.github.com>
2022-01-05 16:27:24 +0200

    removed last commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactGA from "react-ga";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/home/home";
 import AboutUs from "./pages/aboutus/aboutus";
