578f6c5a1536c2e510c2b4db23466adcc5dff556
Monica <32903674+monicaafilip@users.noreply.github.com>
2022-01-17 18:21:41 +0200

    added sales on main page

--- src/components/mycarousel/mycarousel.js
+++ src/components/mycarousel/mycarousel.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { connect } from "react-redux";
 import { useHistory } from "react-router";
 
 import { Carousel } from "react-bootstrap";
@@ -8,12 +9,15 @@ import "./mycarousel.css";
 import firstImg from "../../assets/temporary/products/prod (1).jpeg";
 import secondImg from "../../assets/temporary/products/prod (2).jpeg";
 
-function MyCarousel() {
+function MyCarousel(st) {
   const history = useHistory();
   const routeChange = () => {
     let path = "/produse";
     history.push(path);
   };
+  const salesProducts = st.state.prods.products.filter((product) =>
+    product.filter.includes("reduceri")
+  );
 
   return (
     <Carousel>
@@ -38,7 +42,24 @@ function MyCarousel() {
           </button>
         </Carousel.Caption>
       </Carousel.Item>
+      {salesProducts.map((product) => (
+        <Carousel.Item interval={2000}>
+          <img className="d-block w-100" src={product.imageUrl} alt="slide" />
+          <Carousel.Caption>
+            {" "}
+            <h1 className="display-4 h1VWSize">Mobilă la reducere!</h1>
+            <a className="phone-number" href="tel:+40746 086 433">
+              Rezervă în magazin!
+            </a>
+          </Carousel.Caption>
+        </Carousel.Item>
+      ))}
     </Carousel>
   );
 }
-export default MyCarousel;
+
+function mapStateToProps(state) {
+  return { state };
+}
+
+export default connect(mapStateToProps)(MyCarousel);
