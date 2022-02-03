203060720d49b0acd5e1cc16fe4b72eebce91f96
Monica <32903674+monicaafilip@users.noreply.github.com>
2022-02-03 16:06:53 +0200

    added logs

--- src/redux/directory/directory.reducer.jsx
+++ src/redux/directory/directory.reducer.jsx
@@ -14,6 +14,7 @@ const directoryReducer = (state = INITIAL_STATE, action) => {
         return product.filter.toLowerCase().includes(category);
       });
 
+      console.log(filteredValues);
       return updateState(
         state,
         newState,
